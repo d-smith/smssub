ae7cb3e56dff8bb5f6fe0b1e3decee53901426b3
d-smith <doug.smith.mail@gmail.com>
2018-01-17 11:10:44 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# smssub
+
+Dependencies:
+
+<pre>
+go get github.com/aws/aws-lambda-go/lambda
+</pre>
---
+++ smssub.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	ErrBodyNotProvided = errors.New("no HTTP body")
+)
+
+
+type SubRequest struct {
+	InstanceID string `json:instance`
+	Notify     string `json:notify`
+}
+
+type SubResponse struct {
+	Confirmation string
+}
+
+// Handler is your Lambda function handler
+// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
+// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+
+	// If no name is provided in the HTTP request body, throw an error
+	if len(request.Body) < 1 {
+		return events.APIGatewayProxyResponse{}, ErrBodyNotProvided
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello " + request.Body,
+		StatusCode: 200,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(Handler)
+}
